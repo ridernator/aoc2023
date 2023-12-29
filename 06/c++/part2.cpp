e3dc3b54482ffa359aec7b134afe38cb6d8dca79
Ciaron Rider <ciaron@ridernator.com>
2023-12-29 21:29:32 +0000

    Added solution for day 6 part 1

---
+++ 06/c++/part2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+#define INPUT "../data/input"
+
+/**
+ * Read a file entirely into a stringstream
+ * 
+ * @param The name of the file to read
+ * @return The contents of the file
+ **/
+std::stringstream readFile(const std::string& filename = INPUT) {
+    std::ifstream fileStream(filename);
+    std::stringstream returnVal;
+    
+    returnVal << fileStream.rdbuf();
+
+    return returnVal;
+}
+
+/**
+ * Read a file entirely into a vector of strings
+ * 
+ * @param The name of the file to read
+ * @return The vector of lines in the file
+ **/
+std::vector<std::string> readFileToVector(const std::string& filename = INPUT) {
+    std::ifstream fileStream(filename);
+    std::string string;
+    std::vector<std::string> returnVal;
+    
+    while (std::getline(fileStream, string)) {
+        returnVal.push_back(string);
+    }
+
+    return returnVal;
+}
+
+int main() {
+    return 0;
+}
