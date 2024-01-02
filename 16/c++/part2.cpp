063028944644d3f8b30fa51660887a7bda12465a
Ciaron Rider <ciaron@ridernator.com>
2024-01-02 17:43:01 +0000

    Added solution to day 16 part 2

--- 16/c++/part2.cpp
+++ 16/c++/part2.cpp
@@ -1,4 +1,8 @@
+#include <algorithm>
+#include <cinttypes>
+#include <cstdint>
 #include <iostream>
+#include <map>
 #include <string>
 #include <sstream>
 #include <fstream>
@@ -8,14 +12,14 @@
 
 /**
  * Read a file entirely into a stringstream
- * 
+ *
  * @param The name of the file to read
  * @return The contents of the file
  **/
 std::stringstream readFile(const std::string& filename = INPUT) {
     std::ifstream fileStream(filename);
     std::stringstream returnVal;
-    
+
     returnVal << fileStream.rdbuf();
 
     return returnVal;
@@ -23,7 +27,7 @@ std::stringstream readFile(const std::string& filename = INPUT) {
 
 /**
  * Read a file entirely into a vector of strings
- * 
+ *
  * @param The name of the file to read
  * @return The vector of lines in the file
  **/
@@ -31,7 +35,7 @@ std::vector<std::string> readFileToVector(const std::string& filename = INPUT) {
     std::ifstream fileStream(filename);
     std::string string;
     std::vector<std::string> returnVal;
-    
+
     while (std::getline(fileStream, string)) {
         returnVal.push_back(string);
     }
@@ -39,6 +43,227 @@ std::vector<std::string> readFileToVector(const std::string& filename = INPUT) {
     return returnVal;
 }
 
+enum class Direction {
+    N,
+    E,
+    S,
+    W
+};
+
+void loop(const int64_t startX,
+          const int64_t startY,
+          const Direction startDirection,
+          const std::vector<std::string>& grid,
+          std::map<Direction, bool**>& already) {
+    int64_t x = startX;
+    int64_t y = startY;
+    Direction direction = startDirection;
+
+    while (true) {
+        if ((x < 0) ||
+            (y < 0) ||
+            ((std::size_t) x == grid[0].size()) ||
+            ((std::size_t) y == grid.size()) ||
+            (already[direction][y][x])) {
+            break;
+        }
+
+        already[direction][y][x] = true;
+
+        switch (grid[y][x]) {
+            case '/': {
+                switch (direction) {
+                    case Direction::N: {
+                        direction = Direction::E;
+
+                        break;
+                    }
+
+                    case Direction::E: {
+                        direction = Direction::N;
+
+                        break;
+                    }
+
+                    case Direction::S: {
+                        direction = Direction::W;
+
+                        break;
+                    }
+
+                    case Direction::W: {
+                        direction = Direction::S;
+
+                        break;
+                    }
+                }
+
+                break;
+            }
+
+            case '\\': {
+                switch (direction) {
+                    case Direction::N: {
+                        direction = Direction::W;
+
+                        break;
+                    }
+
+                    case Direction::E: {
+                        direction = Direction::S;
+
+                        break;
+                    }
+
+                    case Direction::S: {
+                        direction = Direction::E;
+
+                        break;
+                    }
+
+                    case Direction::W: {
+                        direction = Direction::N;
+
+                        break;
+                    }
+                }
+
+                break;
+            }
+
+            case '-': {
+                switch (direction) {
+                    case Direction::N:
+                    case Direction::S: {
+                        loop(x + 1, y, Direction::E, grid, already);
+
+                        direction = Direction::W;
+
+                        break;
+                    }
+
+                    default: {
+
+                        break;
+                    }
+                }
+
+                break;
+            }
+
+            case '|': {
+                switch (direction) {
+                    case Direction::E:
+                    case Direction::W: {
+                        loop(x, y + 1, Direction::S, grid, already);
+
+                        direction = Direction::N;
+
+                        break;
+                    }
+
+                    default: {
+
+                        break;
+                    }
+                }
+
+                break;
+            }
+
+            default: {
+                break;
+            }
+        }
+
+        switch (direction) {
+            case Direction::N: {
+                --y;
+
+                break;
+            }
+
+            case Direction::E: {
+                ++x;
+
+                break;
+            }
+
+            case Direction::S: {
+                ++y;
+
+                break;
+            }
+
+            case Direction::W: {
+                --x;
+
+                break;
+            }
+        }
+    }
+}
+
+uint64_t tryHard(const uint64_t startX,
+                 const uint64_t startY,
+                 const Direction direction) {
+    auto grid = readFileToVector();
+    const std::size_t maxX = grid[0].size();
+    const std::size_t maxY = grid.size();
+
+    std::map<Direction, bool**> already;
+
+    bool** northAlready = new bool*[maxY];
+    bool** eastAlready = new bool*[maxY];
+    bool** southAlready = new bool*[maxY];
+    bool** westAlready = new bool*[maxY];
+    for (std::size_t index = 0; index < maxY; ++index) {
+        *(northAlready + index) = new bool[maxX];
+        *(eastAlready + index) = new bool[maxX];
+        *(southAlready + index) = new bool[maxX];
+        *(westAlready + index) = new bool[maxX];
+        for (std::size_t index2 = 0; index2 < maxX; ++index2) {
+            *(*(northAlready + index) + index2) = false;
+            *(*(eastAlready + index) + index2) = false;
+            *(*(southAlready + index) + index2) = false;
+            *(*(westAlready + index) + index2) = false;
+        }
+    }
+    already[Direction::N] = northAlready;
+    already[Direction::E] = eastAlready;
+    already[Direction::S] = southAlready;
+    already[Direction::W] = westAlready;
+
+    loop(startX, startY, direction, grid, already);
+
+    uint64_t energizedTiles = 0;
+    for (std::size_t index = 0; index < maxY; ++index) {
+        for (std::size_t index2 = 0; index2 < maxX; ++index2) {
+            if ((*(*(northAlready + index) + index2)) ||
+                (*(*(eastAlready + index) + index2)) ||
+                (*(*(southAlready + index) + index2)) ||
+                (*(*(westAlready + index) + index2))) {
+                ++energizedTiles;
+            }
+        }
+    }
+
+    return energizedTiles;
+}
+
 int main() {
-    return 0;
+    auto grid = readFileToVector();
+    uint64_t best = 0;
+
+    for (uint64_t index = 0; index < grid.size(); ++index) {
+        best = std::max(best, tryHard(0, index, Direction::E));
+        best = std::max(best, tryHard(grid[0].size() - 1, index, Direction::W));
+    }
+
+    for (uint64_t index = 0; index < grid[0].size(); ++index) {
+        best = std::max(best, tryHard(index, 0, Direction::S));
+        best = std::max(best, tryHard(index, grid.size() - 1, Direction::N));
+    }
+
+    std::cout << "Max number of energized tiles = " << best << std::endl;
 }
